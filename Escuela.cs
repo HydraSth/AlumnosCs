78c13295771c85742797587dd43d07472cbe9175
Juan Cruz <juanespasan@gmail.com>
2023-05-31 23:51:38 -0300

    Version 1.0 | Msj:Solucion de multiples if anidados con comprobacion

--- Escuela.cs
+++ Escuela.cs
@@ -8,9 +8,10 @@ namespace CrearEscuela
         public List<int> grado = new List<int> { 0 };
         public List<int> vacantes = new List<int> { 0 };
         public int indice = 2;
-        public int indiceError = 2;
-        public static SLDocument CrearVacantes(){
-            SLDocument XLSX= new SLDocument();
+
+        public static SLDocument CrearVacantes()
+        {
+            SLDocument XLSX = new SLDocument();
             XLSX.SetCellValue(1, 1, "nro_inscripcion");
             XLSX.SetCellValue(1, 2, "nombre");
             XLSX.SetCellValue(1, 3, "edad");
@@ -46,27 +47,29 @@ namespace CrearEscuela
             }
             return false;
         }
-        public Tuple<SLDocument,bool> datosActualizado(Alumno obj_alumno, SLDocument archivo, Escuela escuela , SLDocument error)
+        public Tuple<SLDocument, bool> datosActualizado(Alumno obj_alumno, SLDocument archivo, Escuela escuela, SLDocument error, int ierror)
         {
-            if(archivo?.GetCellValueAsInt32(indice, 1) != null)
+            if (archivo?.GetCellValueAsInt32(indice, 1) != null)
             {
-                if(escuela.vacantes[obj_alumno.grado] > 0){
+                if (escuela.vacantes[obj_alumno.grado] > 0)
+                {
                     archivo.SetCellValue(indice, 1, obj_alumno.id);
                     archivo.SetCellValue(indice, 2, obj_alumno.nombre);
                     archivo.SetCellValue(indice, 3, obj_alumno.edad);
                     archivo.SetCellValue(indice, 4, obj_alumno.grado);
                     escuela.actualizarAlumnos(obj_alumno.grado);
                     this.indice++;
-                    return Tuple.Create(archivo,true);
-                }else{
-                    error.SetCellValue(indiceError, 1, obj_alumno.id);
-                    error.SetCellValue(indiceError, 2, obj_alumno.nombre);
-                    error.SetCellValue(indiceError, 3, obj_alumno.edad);
-                    error.SetCellValue(indiceError, 4, obj_alumno.grado);
-                    this.indiceError++;
+                    return Tuple.Create(archivo, true);
+                }
+                else
+                {
+                    error.SetCellValue(ierror, 1, obj_alumno.id);
+                    error.SetCellValue(ierror, 2, obj_alumno.nombre);
+                    error.SetCellValue(ierror, 3, obj_alumno.edad);
+                    error.SetCellValue(ierror, 4, obj_alumno.grado);
                 }
             }
-            return Tuple.Create(error,false);
+            return Tuple.Create(error, false);
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 using CrearAlumno;
 using CrearEscuela;
 
-//Leer incripciones
 string doc_incripcione = @"C:\Users\Usuario\Desktop\Evaluativo_Practica\DATA\Inscripciones.xlsx";
 SLDocument incripciones = new SLDocument(doc_incripcione);
 List<Alumno> Alumnos = new List<Alumno>();
+
 int fila = 2;
 while (!string.IsNullOrEmpty(incripciones.GetCellValueAsString(fila, 1)))
 {
@@ -26,76 +26,109 @@ SLDocument VacantesToay = Escuela.CrearVacantes();
 SLDocument VacantesSRosa = Escuela.CrearVacantes();
 SLDocument VacantesAnguil = Escuela.CrearVacantes();
 
-SLDocument VacantesError_Toay = Escuela.CrearVacantes();
-SLDocument VacantesError_SRosa = Escuela.CrearVacantes();
-SLDocument VacantesError_Anguil = Escuela.CrearVacantes();
-
+SLDocument VacantesListaDeEspera = Escuela.CrearVacantes();
+int indiceError = 2;
 foreach (var alumno in Alumnos)
 {
     switch (alumno.preferencia)
     {
         case "Nro. 1 - Toay":
-            var resToay=Escuela_Toay.datosActualizado(alumno,VacantesToay,Escuela_Toay, VacantesError_Toay);
-            if(resToay.Item2){
-                VacantesToay=resToay.Item1;
-            }else{
-                resToay=Escuela_SRosa.datosActualizado(alumno,VacantesSRosa,Escuela_SRosa, VacantesError_Toay);
-                if(resToay.Item2){
-                    VacantesSRosa=resToay.Item1;
-                }else{
-                    resToay=Escuela_Anguil.datosActualizado(alumno,VacantesAnguil,Escuela_Anguil, VacantesError_Toay);
-                    if(resToay.Item2){
-                        VacantesAnguil=resToay.Item1;
-                    }else{
-                        VacantesError_Toay=resToay.Item1;
-                    }
-                }
+            var respuesta_vacante = Escuela_Toay.datosActualizado(alumno, VacantesToay, Escuela_Toay, VacantesListaDeEspera, indiceError);
+            if (respuesta_vacante.Item2)
+            {
+                VacantesToay = respuesta_vacante.Item1;
             }
-            break; 
+            else
+            {
+                VacantesListaDeEspera = respuesta_vacante.Item1;
+                indiceError++;
+            }
+            break;
         case "Nro. 2 - Santa Rosa":
-            var resRsa=Escuela_SRosa.datosActualizado(alumno,VacantesSRosa,Escuela_SRosa, VacantesError_SRosa);
-            if(resRsa.Item2){
-                VacantesSRosa=resRsa.Item1;
-            }else{
-                resRsa=Escuela_Toay.datosActualizado(alumno,VacantesToay,Escuela_Toay, VacantesError_SRosa);
-                if(resRsa.Item2){
-                    VacantesToay=resRsa.Item1;
-                }else{
-                    resRsa=Escuela_Anguil.datosActualizado(alumno,VacantesAnguil,Escuela_Anguil, VacantesError_SRosa);
-                    if(resRsa.Item2){
-                        VacantesAnguil=resRsa.Item1;
-                    }else{
-                        VacantesError_SRosa=resRsa.Item1;
-                    }
-                }
+            respuesta_vacante = Escuela_SRosa.datosActualizado(alumno, VacantesSRosa, Escuela_SRosa, VacantesListaDeEspera, indiceError);
+            if (respuesta_vacante.Item2)
+            {
+                VacantesSRosa = respuesta_vacante.Item1;
+            }
+            else
+            {
+                VacantesListaDeEspera = respuesta_vacante.Item1;
+                indiceError++;
             }
             break;
         case "Nro. 3 - Anguil":
-            var resAnguil=Escuela_Anguil.datosActualizado(alumno,VacantesAnguil,Escuela_Anguil, VacantesError_Anguil);
-            if(resAnguil.Item2){
-                VacantesAnguil=resAnguil.Item1;
-            }else{
-                resAnguil=Escuela_SRosa.datosActualizado(alumno,VacantesSRosa,Escuela_SRosa, VacantesError_Anguil);
-                if(resAnguil.Item2){
-                    VacantesSRosa=resAnguil.Item1;
-                }else{
-                    resAnguil=Escuela_Toay.datosActualizado(alumno,VacantesToay,Escuela_Toay, VacantesError_Anguil);
-                    if(resAnguil.Item2){
-                        VacantesToay=resAnguil.Item1;
-                    }else{
-                        VacantesError_Anguil=resAnguil.Item1;
-                    }
-                }
+            respuesta_vacante = Escuela_Anguil.datosActualizado(alumno, VacantesAnguil, Escuela_Anguil, VacantesListaDeEspera, indiceError);
+            if (respuesta_vacante.Item2)
+            {
+                VacantesAnguil = respuesta_vacante.Item1;
+            }
+            else
+            {
+                VacantesListaDeEspera = respuesta_vacante.Item1;
+                indiceError++;
             }
             break;
     }
 }
 
+List<Alumno> AlumnosEnListaDeEspera = new List<Alumno>();
+fila = 2;
+while (!string.IsNullOrEmpty(VacantesListaDeEspera.GetCellValueAsString(fila, 1)))
+{
+    int id = VacantesListaDeEspera.GetCellValueAsInt32(fila, 1);
+    string nombre = VacantesListaDeEspera.GetCellValueAsString(fila, 2);
+    int edad = VacantesListaDeEspera.GetCellValueAsInt32(fila, 3);
+    int grado = VacantesListaDeEspera.GetCellValueAsInt32(fila, 4);
+    string preferencia = "x";
+    AlumnosEnListaDeEspera.Add(new Alumno(id, nombre, edad, grado, preferencia));
+    fila++;
+}
+
+VacantesListaDeEspera = Escuela.CrearVacantes();
+indiceError = 2;
+foreach (var alumno in AlumnosEnListaDeEspera)
+{
+    //Prueba con escuela Toay
+    var respuesta_vacante = Escuela_Toay.datosActualizado(alumno, VacantesToay, Escuela_Toay, VacantesListaDeEspera, indiceError);
+    if (respuesta_vacante.Item2)
+    {
+        VacantesToay = respuesta_vacante.Item1;
+    }
+    else
+    {
+        //Prueba con escuela Anguil
+        respuesta_vacante = Escuela_Anguil.datosActualizado(alumno, VacantesAnguil, Escuela_Anguil, VacantesListaDeEspera, indiceError);
+        if (respuesta_vacante.Item2)
+        {
+            VacantesAnguil = respuesta_vacante.Item1;
+        }
+        else
+        {
+            //Prueba con escuela Santa Rosa
+            respuesta_vacante = Escuela_Toay.datosActualizado(alumno, VacantesToay, Escuela_Toay, VacantesListaDeEspera, indiceError);
+            if (respuesta_vacante.Item2)
+            {
+                VacantesToay = respuesta_vacante.Item1;
+            }
+            else
+            {
+                //No pudo ser ingresado en ningun colegio queda en lista de espera
+                VacantesListaDeEspera = respuesta_vacante.Item1;
+                indiceError++;
+            }
+        }
+    }
+}
+
+//Chequeo para mostrar las vacantes
+// for (int i = 0; i < 8; i++){
+//     System.Console.WriteLine($"A la escuela santa rosa grado {Escuela_SRosa.grado[i]} le quedan {Escuela_SRosa.vacantes[i]}");
+//     System.Console.WriteLine($"A la escuela toay grado {Escuela_Toay.grado[i]} le quedan {Escuela_Toay.vacantes[i]}");
+//     System.Console.WriteLine($"A la escuela anguil grado {Escuela_Anguil.grado[i]} le quedan {Escuela_Anguil.vacantes[i]}");
+// }
+
 VacantesToay.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Vacantes_ColegioToay.xlsx");
 VacantesSRosa.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Vacantes_ColegioSRosa.xlsx");
 VacantesAnguil.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Vacantes_ColegioAnguil.xlsx");
 
-VacantesError_Toay.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Error\VacantesError_ColegioToay.xlsx");
-VacantesError_SRosa.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Error\VacantesError_ColegioSRosa.xlsx");
-VacantesError_Anguil.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Error\VacantesError_ColegioAnguil.xlsx");
-
+VacantesListaDeEspera.SaveAs(@"C:\Users\Usuario\Desktop\Evaluativo_Practica\Vacantes\Error\VacantesListaDeEspera.xlsx");
